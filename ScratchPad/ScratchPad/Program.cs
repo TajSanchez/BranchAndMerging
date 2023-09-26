ec85e96d68ca3056f0642f39afa7b65025b61e3d
Taj <tajmarie92@gmail.com>
2023-09-26 09:22:56 -0500

    added line 1

---
+++ ScratchPad/ScratchPad/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ScratchPad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User! Please tell me your name & age!");
+
+            Console.WriteLine("Please Enter Your Name Here");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Your Age Here");
+            int age = int.Parse(Console.ReadLine());
+
+            //string.StartsWith() OR []            
+
+            if (age >= 18)
+            {
+                Console.WriteLine($"Well hello {userName} you are {age} years old");
+
+                if (userName[0] == 'T')
+                {
+                    for (var i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine(userName);
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"Hello little kid.");
+            }
+
+        }
+    }
+}
